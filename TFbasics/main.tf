cf8e8985e35a9b33c02029787d3ae23779384a50
Hrushi Salgaonkar <salgaonkar.hrushi@gmail.com>
2025-08-15 19:24:14 +0530

    first change in new branch

--- TFbasics/main.tf
+++ TFbasics/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "aws_instance" "MyIncident1" {
   ami           = "ami-0d54604676873b4ec"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "MyIncident"
