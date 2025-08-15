0213383e80c4e29948e959a40c47b8641af3fab6
Hrushi Salgaonkar <salgaonkar.hrushi@gmail.com>
2025-08-15 18:20:26 +0530

    name change for ec2

--- TFbasics/main.tf
+++ TFbasics/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.main_Region
 }
 
-resource "aws_instance" "MyIncident" {
+resource "aws_instance" "MyIncident1" {
   ami           = "ami-0d54604676873b4ec"
   instance_type = "t2.micro"
 
