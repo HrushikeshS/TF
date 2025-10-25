781925da48dec04199a7cc0186a387379dc0c6b0
Hrushi Salgaonkar <salgaonkar.hrushi@gmail.com>
2025-10-25 18:11:20 +0530

    unkown changes

--- Jenkin/main.tf
+++ Jenkin/main.tf
@@ -4,7 +4,7 @@
     }
 
     # Create a Security Group for SSH and HTTP access
-    resource "aws_security_group" "jenkins_sg1" {
+    resource "aws_security_group" "jenkins_sg" {
       name        = "jenkins-security-group"
       description = "Allow SSH and HTTP traffic"
       ingress {
